cf92f85fc4510d10a0908ffc5e93690929db0e53
ananyagoswami18 <ananyagoswami2021@gmail.com>
2025-04-27 16:31:25 +0530

    Initial Commit

---
+++ src/main/java/com/api/synchronyApiCodingChallenge/SynchronyApiCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.api.synchronyApiCodingChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchronyApiCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynchronyApiCodingChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/synchronyApiCodingChallenge/SynchronyApiCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.synchronyApiCodingChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchronyApiCodingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
